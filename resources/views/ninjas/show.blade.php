f18bc0ffd9151b6ba86bb19be4a0491371ef5d1e
Kyle Quilty <kquilty@gmail.com>
2025-09-26 12:57:45 -0400

    animated main list

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
     <h1>{{ $ninja->name }}, {{ $ninja->age }}</h1>
 
-    <div class="p-4 bg-gray-100 rounded shadow-md">
+    <div class="p-4 bg-white rounded shadow-md mt-4">
         <p><strong>Skill Level:</strong> {{ $ninja->skill }}</p>
         <p><strong>About Me:</strong></p>
         <p>{{ $ninja->bio }}</p>
     </div>
-    <div class="p-4 bg-gray-100 rounded shadow-md mt-4">
+    <div class="p-4 bg-white rounded shadow-md mt-4">
         <strong>Dojo:</strong> {{ $ninja->dojo->name }}
         <p><strong>Location:</strong> {{ $ninja->dojo->location }}</p>
     </div>
